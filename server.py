16afdd04f3fe87dd68805ffac4ac91309ed27385
NhorkSopheakna <sopheaknanhork@gmail.com>
2022-07-07 17:41:09 +0700

    11.2

--- server.py
+++ server.py
@@ -5,3 +5,12 @@ sock.connect (('172.16.1.96', port))
 msg = sock.recv(1024)
 msg
 
+msg_decoded = msg.decode('utf_8')
+msg_decoded
+
+print('Message From Server: ' + msg_decoded)
+
+msg_back = input('Do you Want To The Server ?')
+msg_back_encoded = msg_back.encode('utf-8')
+sock.send(msg_back_encoded)
+
